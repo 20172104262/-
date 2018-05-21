2616cdb285bd76c5146f8ffb00246b609a6529f5
20172104262 <39482046+20172104262@users.noreply.github.com>
2018-05-21 17:28:37 +0800

    Initial commit

---
+++ 英尺的转换/英尺的转换.cpp
@@ -0,0 +1,56 @@
+// 英尺的转换.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+class Britishlength
+{
+public:
+	double feet;
+	double inch;
+	int feet2;
+	int inch2;
+	int mater;
+	int feet3;
+	double inch3;
+	double a;
+	int b;
+	void import()
+	{
+		cin >> mater;
+		cin >> feet2 >> inch2;
+	}
+	void transtion()
+	{
+		a = mater * 3.2808399;
+		feet = (int)a;
+		inch = (a - feet) * 12;
+	}
+	void arithmetic()
+	{
+		inch3 = inch + inch2;
+		if (inch3 >= 12)
+		{
+			b = inch3 / 12;
+			inch3 = inch3 - b * 12;
+		}
+		feet3 = feet2 + b + feet;
+	}
+	void display()
+	{
+		cout << feet3 << "feet" << inch3 << "inch" << endl;
+	}
+};
+
+int main()
+{
+	Britishlength oc;
+	oc.import();
+	oc.transtion();
+	oc.arithmetic();
+	oc.display();
+	return 0;
+}
+
+
